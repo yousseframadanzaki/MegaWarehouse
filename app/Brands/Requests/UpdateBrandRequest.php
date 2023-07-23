591f89743a34aec325d44aabf4803289e42a1b61
youssef ramadan <yousseframadanzaki@gmail.com>
2023-07-23 11:02:11 +0300

    added Rule to brands and Categories

--- app/Brands/Requests/UpdateBrandRequest.php
+++ app/Brands/Requests/UpdateBrandRequest.php
@@ -3,6 +3,7 @@
 namespace App\Brands\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateBrandRequest extends FormRequest
 {
--- app/Categories/Requests/UpdateCategoryRequest.php
+++ app/Categories/Requests/UpdateCategoryRequest.php
@@ -3,6 +3,7 @@
 namespace App\Categories\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateCategoryRequest extends FormRequest
 {
