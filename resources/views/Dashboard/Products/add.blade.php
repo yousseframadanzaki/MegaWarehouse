3e12331e69d48ceaad932001705e4782b1669443
youssef ramadan <yousseframadanzaki@gmail.com>
2023-08-14 12:29:42 +0300

    added new image plugin

--- resources/views/Dashboard/Products/add.blade.php
+++ resources/views/Dashboard/Products/add.blade.php
@@ -24,7 +24,7 @@
                 <li>اضافة منتج جديد</li>
             </ul>
         </div>
-        <div class="row  needs-validation " novalidate>
+        <form class="row  needs-validation " enctype="multipart/form-data">
             @csrf
             <div class="card p-3 shadow-sm">
                 <h3 class="text-center">أضافة منتج جديد</h3>
@@ -131,6 +131,11 @@
 
                 <div class="row mb-3">
                     <h3 class="form-label">صور المنتج <i class="bi bi-images"></i></h3>
+                    <div class="input-images"></div>
+                </div>
+
+                {{-- <div class="row mb-3">
+                    <h3 class="form-label">صور المنتج <i class="bi bi-images"></i></h3>
                     <div class="col-md-12">
                         <form class="dropzone" id="product-form" action="{{ route('store_product') }}" method="POST"
                             enctype="multipart/form-data">
@@ -142,7 +147,7 @@
                             <div class="dz-message" data-dz-message><span>قم بالضغط لرفع الصور</span></div>
                         </form>
                     </div>
-                </div>
+                </div> --}}
 
                 <div class="row mb-3 mt-2">
                     <h3>اختيارات المنتج <i class="bi bi-list-ul"></i></h3>
@@ -183,12 +188,284 @@
                 <option value="اللون">
                 <option value="الخامة">
             </datalist>
-    </div>
+        </form>
     <meta name="_token" content="{{ csrf_token() }}">
     @endsection
 @section('script')
+<script type="text/javascript" src="{{url('/static/js/image-uploader.js')}}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js" integrity="sha512-2ImtlRlf2VVmiGZsjm9bEyhjGW4dU7B6TNwh/hx/iSByxNENtj3WVE6o/9Lj4TJeVXPi4bnOIMXFIJJAeufa0A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script>
+<script>
+    $(document).ready(function() {
+        $('#summernote').summernote({
+            height: 200,
+        });
+        $('select.product_info').select2({
+            padding: 'resolve',
+        });
+        $('.input-images').imageUploader();
+    })
+    form_options_array = [];
+    form_options_values = new Object();
+    options = {};
+
+    $("#add_option_btn").click(function(e) {
+        e.preventDefault();
+        var options_count = form_options_array.length;
+        add_option();
+        if (options_count >= 0) {
+            $('#save_options_btn').fadeIn();
+        }
+        if (options_count + 1 == 3) {
+            $(this).fadeOut();
+        }
+    })
+    function add_option() {
+        var option_id = uid();
+        form_options_array.push(option_id);
+        var option_template = `
+        <div class="option row mb-3 border shadow-sm p-3" id="` + option_id + `">
+            <div class="col-md-5">
+                <label class="form-label">اسم الاختيار <span class="text-danger">*</span></label>
+                <input type="text" class="form-control option_name"  list="default_options">
+                <div class="invalid-feedback">
+                    برجاء أضافة اسم الاختيار
+                </div>
+            </div>
+            <div class="col-md-5 values_display" style="display:none;">
+                <label class="form-label">قيم الاختيار:-</label> 
+                <div class="option_values_div">
+
+                </div> 
+            </div>
+            <div class="col-md-5 option_value_div">
+                <label class="form-label">قيم الاختيار <span class="text-danger">*</span></label>
+                <input type="text" class="form-control option_values" data-option-id="` + option_id + `" placeholder="برجاء ادخال القيمة والضغط على زر enter">
+                <div id="invalid-` + option_id + `" class="invalid-feedback">
+                    برجاء أضافة قيم الاختيار
+                </div>
+            </div>
+            <div class="col-md-2 d-md-flex align-items-end">
+                <a class="remove_option btn btn-danger" data-remove-id="` + option_id + `"><i class="bi bi-trash"></i></a>
+            </div>
+            <div class="row mt-3 values_edit">
+                <label class="form-label">قيم الاختيار:-</label> 
+                <div class="option_values_div">
+
+                </div> 
+            </div>
+        </div>
+        `;
+        $('.options').append(option_template);
+    }
+    const uid = function() {
+        return Date.now().toString(36) + Math.random().toString(36).substr(2);
+    }
+    $(document).on("click", ".remove_option", function(e) {
+        e.preventDefault();
+        var remove_id = $(this).attr("data-remove-id");
+        $("#" + remove_id).fadeOut();
+        $("#" + remove_id).remove();
+        delete form_options_values[remove_id];
+        delete options[remove_id];
+        update_form_options();
+        const index = form_options_array.indexOf(remove_id);
+
+        if (index > -1) {
+            form_options_array.splice(index, 1);
+        }
+        if (form_options_array.length < 3) {
+            $("#add_option_btn").fadeIn();
+        }
+        var options_count = form_options_array.length;
+        if (options_count === 0) {
+            $('#save_options_btn').fadeOut();
+            $('#variants_table tbody').html("");
+            $('#variants_table').fadeOut();
+        }
+    });
+    $(document).on("keypress", ".option_name", function(e) {
+            if (e.keyCode == 13) {
+                e.preventDefault();
+            }
+        })
+
+        $(document).on("keypress", ".option_values", function(e) {
+            if (e.keyCode == 13) {
+                e.preventDefault();
+                var value = $(this).val();
+                if (!value) {
+                    return false;
+                }
+                var option_id = $(this).attr('data-option-id');
+                if (form_options_values[option_id]) {
+                    form_options_values[option_id].push(value);
+                } else {
+                    form_options_values[option_id] = [value];
+                }
+                $(this).val("");
+                update_form_options();
+            }
+        });
+
+        function update_form_options() {
+            $('.option_values_div').html('')
+            Object.keys(form_options_values).forEach(key => {
+                form_options_values[key].forEach(value => {
+                    var template = `
+                <div 
+                    class="remove_option_value hover-danger btn btn-success option_value_` + key + `" 
+                    style="margin-left:10px" 
+                    data-value="${value}"
+                    data-option-id="${key}"
+                >
+                    ${value}
+                    <i class="bi bi-trash"></i>
+                </div>`
+                    $('#' + key + ' .option_values_div').append(template);
+                });
+            });
+        }
+
+        $(document).on("click", ".remove_option_value", function(e) {
+            var option_id = $(this).attr("data-option-id");
+            var value = $(this).attr("data-value");
+
+            const index = form_options_values[option_id].indexOf(value);
+            if (index > -1) {
+                form_options_values[option_id].splice(index, 1);
+            }
+            update_form_options();
+        })
+
+        $(document).on("click", "#save_options_btn", function(e) {
+            e.preventDefault();
+
+
+            if (!options_data_valid()) {
+                return;
+            }
+
+            form_options_array.forEach(option_id => {
+                var option_name = $('#' + option_id + ' .option_name').val();
+                options[option_id] = {
+                    option_name: option_name,
+                    option_values: form_options_values[option_id]
+                };
+            });
+
+            generate_variants();
+
+            $(".option_value_div").fadeOut();
+            $(".remove_option").fadeOut();
+            $(".remove_option_value i").fadeOut();
+            $(".remove_option_value").removeClass('hover-danger');
+            $(".option_name").prop('readonly', true);
+            $(".remove_option_value").prop('disabled', true);
+            $("#add_option_btn").fadeOut();
+            $(".values_edit").fadeOut();
+            $(".values_display").fadeIn();
+
+            $("#edit_options_btn").fadeIn();
+            $(this).fadeOut();
+
+        })
+
+        $(document).on("click", "#edit_options_btn", function(e) {
+            e.preventDefault();
+            $(".option_value_div").fadeIn();
+            $(".remove_option").fadeIn();
+            $(".remove_option_value i").fadeIn();
+            $(".remove_option_value").addClass('hover-danger');
+            $(".option_name").prop('readonly', false);
+            $(".remove_option_value").prop('disabled', false);
+            $("#add_option_btn").fadeIn();
+            $(".values_edit").fadeIn();
+            $(".values_display").fadeOut();
+            $("#save_options_btn").fadeIn();
+            $(this).fadeOut();
+            console.log(options);
+        })
+
+        function options_data_valid() {
+            var valid = true;
+            form_options_array.forEach(option_id => {
+                var option_name = $('#' + option_id + ' .option_name').val();
+                var option_values = form_options_values[option_id];
+
+                if (!option_name) {
+                    valid = false;
+                    $("#" + option_id + " .invalid-feedback").fadeIn();
+                    $("#" + option_id + " .option_name").addClass("is-invalid");
+                } else {
+                    $("#" + option_id + " .invalid-feedback").fadeOut();
+                    $("#" + option_id + " .option_name").removeClass("is-invalid");
+                }
+
+
+                if (option_values === undefined || option_values.length == 0) {
+                    valid = false;
+                    $("#" + option_id + " #invalid-" + option_id).fadeIn();
+                    $("#" + option_id + " .option_values").addClass("is-invalid");
+                } else {
+                    $("#" + option_id + " #invalid-" + option_id).fadeOut();
+                    $("#" + option_id + " .option_values").removeClass("is-invalid");
+                }
+
+            });
+            return valid;
+        }
+
+        function generate_variants() {
+            var variants = [];
+
+            var attributes = {};
+
+            Object.entries(options).forEach(element => {
+                attributes[element[1].option_name] = element[1].option_values;
+            });
+
+            for (const [attr, values] of Object.entries(attributes))
+                variants.push(values.map(v => ({
+                    [attr]: v
+                })));
+
+            variants = variants.reduce((a, b) => a.flatMap(d => b.map(e => ({
+                ...d,
+                ...e
+            }))));
+
+            variants.forEach(variant => {
+                variant.name = Object.keys(variant).map(key => variant[key]).join('-');
+                // console.log(variant);
+            });
+
+            add_variants_to_table(variants);
+
+        }
+
+        function add_variants_to_table(variants) {
+            $("#variants_table tbody").html("");
+            variants.forEach((element, index) => {
+                var template = `
+                <tr>
+                <td>` + element.name + `</td>
+                <td><input class=" product_variant form-control" name="product_variants[` + index + `][price]" type="number"/></td>
+                <td><input class="sku product_variant form-control" name="product_variants[` + index + `][sku]" type="text"/></td>
+                <input type="hidden" class="product_variant" name="product_variants[` + index +
+                    `][name]" value="` + element.name + `"/>`
+                Object.entries(element).forEach(option_value => {
+                    if (option_value[0] != 'name') {
+                        template += `<input type="hidden" class="product_variant" name="product_variants[` +
+                            index + `][options][` + option_value[0] + `]" value="` + option_value[1] + `"/>`
+                    }
+                });
+                template += `</tr>`;
+                $("#variants_table tbody").append(template);
+            });
+            $("#variants_table").fadeIn();
+        }
+</script>    
+{{-- <script>
         $(document).ready(function() {
             $('#summernote').summernote({
                 height: 200,
@@ -583,5 +860,8 @@
 
         }
 
+    </script> --}}
+    <script>
+        
     </script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" integrity="sha512-nMNlpuaDPrqlEls3IX/Q56H36qvBASwb3ipuo3MxeWbsQB1881ox0cRv7UPTgBlriqoynt35KjEwgGUeUXIPnw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css" integrity="sha512-wR4oNhLBHf7smjy0K4oqzdWumd+r5/+6QO/vDda76MW5iug4PT7v86FoEkySIJft3XA0Ae6axhIvHrqwm793Nw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css" integrity="sha512-6lLUdeQ5uheMFbWm3CP271l14RsX1xtx+J5x2yeIDkkiBpeVTNhTqijME7GgRKKi6hCqovwCoBTlRBEC20M8Mg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" type="text/css" href="{{ url('/static/css/image-uploader.css') }}" />
+
     <style>
         .sidebar{
             overflow-y: auto;
