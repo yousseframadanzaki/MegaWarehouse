cc3dc5a0160466c3d3aadd29143e65e200511702
youssef ramadan <yousseframadanzaki@gmail.com>
2023-08-15 13:19:12 +0300

    changed vite build directory

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: './public/storage/build'
+    },
     plugins: [
         laravel({
             input: [
+                'resources/css/app.css',
                 'resources/sass/app.scss',
+                'resources/sass/_variables.scss',
                 'resources/js/app.js',
+                'resources/js/bootstrap.js',
             ],
             refresh: true,
         }),
