59aaea700d923440e068d8ea38574f188a19cca5
basantali-primo <karim.essam620@gmail.com>
2024-06-06 16:37:01 +0300

    Updates

--- routes/web.php
+++ routes/web.php
@@ -479,9 +479,4 @@ Route::group(['prefix' => 'dashboard','middleware' => ['auth','IsNotAdmin']],fun
     Route::get('campaign/add', [WhatsappController::class,'show_campaign'])
     ->name('show_campaign');
 
-    Route::get('whatsapp/points/add', [WhatsappController::class,'add_points'])
-    ->name('add_points');
-
-    Route::post('whatsapp/points/add', [WhatsappController::class,'store_points'])
-    ->name('store_whatsapp_points');
 });
