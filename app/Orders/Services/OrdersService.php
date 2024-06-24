24d09739b3bfdfcaeb5da6e0fc861ff817906658
Karim620 <modnodfod@gmail.com>
2024-06-24 14:46:27 +0300

    remove add transaction  when order status is success

--- app/Orders/Services/OrdersService.php
+++ app/Orders/Services/OrdersService.php
@@ -107,15 +107,6 @@ class OrdersService implements OrdersServiceInterface{
                     'shipping_company_id'=>$data['shipping_company_id'],
                 )
             );
-        } else if ($data['status_id'] == '45') {
-            $this->TransactionService->AddTransaction(array(
-                'order_id' => $order->id,
-                'value' => $order->total_after_sale,
-                'company_id' => $order->company_id,
-                'commission' => $order->total_marketer_commission,
-                'delivery_cost' => $order->delivery_cost,
-                'payment_type_id' => 2,
-            ));
         }
 
         $id = $this->orders_crud_repository->change_order_status($order_id,$data);
@@ -145,19 +136,6 @@ class OrdersService implements OrdersServiceInterface{
                         'shipping_company_id'=>$data['shipping_company_id'],
                     )
                 );
-            }
-        } else if ($data['status_id'] == '45') {
-            foreach ($data['orders_ids'] as $order_id) {
-                $order = $this->orders_crud_repository->get_order_by_id($order_id);
-
-                $this->TransactionService->AddTransaction(array(
-                    'order_id' => $order->id,
-                    'value' => $order->total_after_sale,
-                    'company_id' => $order->company_id,
-                    'commission' => $order->total_marketer_commission,
-                    'delivery_cost' => $order->delivery_cost,
-                    'payment_type_id' => 2,
-                ));
             }
         }
 
