e7a11975140753c94e09f7ca120b836706f0bf06
MohamedAhmed111 <modnodfod@gmail.com>
2024-07-15 23:20:36 +0300

    ..

--- resources/views/Dashboard/Orders/show_one.blade.php
+++ resources/views/Dashboard/Orders/show_one.blade.php
@@ -297,7 +297,7 @@
                             @foreach ($order->stocks as $item)
                             <tr>
                                 <td @empty($item->variant->name) class="text-danger" @endempty>{{$item->variant->product->name??'تابع لمتغير موجود مسبقا'}}</td>
-                                <td class="@if(empty($item->variant->name)) text-danger @elseif ($item->variant->stock()->where('warehouse_id', $item->warehouse->id)->sum('quantity') <= 0) bg-danger text-white @endif">{{$item->variant->name??'متغير موجود مسبقا'}}</td>
+                                <td class="@if(empty($item->variant->name)) text-danger @elseif ($item->variant->stock()->where('warehouse_id', $item->warehouse->id)->sum('quantity') <= 0 && $order->status_id == 5) bg-danger text-white @endif">{{$item->variant->name??'متغير موجود مسبقا'}}</td>
                                 <td>{{$item->warehouse->name}}</td>
                                 <td @empty($item->variant->name) class="text-danger" @endempty>{{$item->variant->product->supplier->name??'تابع لمتغير موجود مسبقا'}}</td>
                                 <td>{{$item->unit_price}}</td>
