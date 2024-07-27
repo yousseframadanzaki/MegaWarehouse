7a437a4c9f1971669adca927fb85ce7a14cea8b4
Karim620 <modnodfod@gmail.com>
2024-07-27 05:56:50 +0300

    fix

--- app/Orders/Services/OrdersService.php
+++ app/Orders/Services/OrdersService.php
@@ -133,7 +133,7 @@ class OrdersService implements OrdersServiceInterface{
                     'shipping_company_id'=>$data['shipping_company_id'],
                 )
             );
-        } elseif($data['status_id'] == '85' && $order->order_status->where('id', 85)->count() == 0) {
+        } elseif($data['status_id'] == '85' && $order->stocks()->where('type', 'returned_orders')->limit(1)->count() == 0) {
             $details = array('type' => 'returned_orders', 'order_stocks' => $order->stocks);
             $this->StockService->CreateOperation(auth()->user(), $details);
         }
@@ -171,7 +171,8 @@ class OrdersService implements OrdersServiceInterface{
         } elseif($data['status_id'] == '85') {
             foreach ($data['orders_ids'] as $order_id) {
                 $order = $this->orders_crud_repository->get_order_by_id($order_id);
-                if ($order->order_status->where('id', 85)->count() == 0) {
+                if ($order->stocks()->where('type', 'returned_orders')->limit(1)->count() == 0) {
+                    dd('ddd');
                     $details = array('type' => 'returned_orders', 'order_stocks' => $order->stocks);
                     $this->StockService->CreateOperation(auth()->user(), $details);
                 }
